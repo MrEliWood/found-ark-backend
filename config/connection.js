f2f7bf8ed915552887e772db074913fe5fa86ed9
Eli Wood <mreliwood1@gmail.com>
2023-01-13 13:59:10 -0800

    initial setup with gcloud

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,30 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-let sequelize;
+let sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+	dialect: 'mysql',
+	host: '/cloudsql/blog-template-374321:us-west1:fabe',
+	timestamps: false,
+	dialectOptions: {
+		socketPath: '/cloudsql/blog-template-374321:us-west1:fabe'
+	}
+});
 
-if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-    sequelize = new Sequelize(
-        process.env.DB_NAME,
-        process.env.DB_USER,
-        process.env.DB_PASSWORD,
-        {
-            host: 'localhost',
-            dialect: 'mysql',
-            port: 3306
-        }
-    );
-}
+// let sequelize;
+
+// if (process.env.JAWSDB_URL) {
+//     sequelize = new Sequelize(process.env.JAWSDB_URL);
+// } else {
+//     sequelize = new Sequelize(
+//         process.env.DB_NAME,
+//         process.env.DB_USER,
+//         process.env.DB_PASSWORD,
+//         {
+//             host: 'localhost',
+//             dialect: 'mysql',
+//             port: 3306
+//         }
+//     );
+// }
 
 module.exports = sequelize;
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const routes = require('./routes');
-require("dotenv").config();
+require('dotenv').config();
 
 const sequelize = require('./config/connection');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
@@ -17,37 +17,41 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-const server = require('http').Server(app)
+const server = require('http').Server(app);
 
-const io = require("socket.io")(server, {
-  cors: {
-    origins: ["http://localhost:3000", "https://found-ark.herokuapp.com", "http://found-ark.herokuapp.com"]
-    //origin: "https://found-ark.herokuapp.com"
-  },
+const io = require('socket.io')(server, {
+	cors: {
+		origins: ['http://localhost:3000', 'https://found-ark.herokuapp.com', 'http://found-ark.herokuapp.com']
+		//origin: "https://found-ark.herokuapp.com"
+	}
 });
 
-io.on("connection", (socket) => {
-  console.log("Connected to socket.io");
-  socket.on("setup", (id) => {
-    socket.join(id);
-    console.log("==============================")
-    console.log(id);
-    socket.emit("connected");
-  });
+io.on('connection', (socket) => {
+	console.log('Connected to socket.io');
+	socket.on('setup', (id) => {
+		socket.join(id);
+		console.log('==============================');
+		console.log(id);
+		socket.emit('connected');
+	});
 
-  socket.on("new notification", (newNoti) => {
-    //if (!newNoti.reciever) return console.log("no reciever");
-    socket.to(newNoti.receiver_id).emit("message recieved", newNoti);
-    // socket.in(newNoti.receiver).emit("message recieved", newNoti);
-    // socket.broadcast.emit('message recieved', newNoti.receiver+" " +newNoti.message)
-  });
+	socket.on('new notification', (newNoti) => {
+		//if (!newNoti.reciever) return console.log("no reciever");
+		socket.to(newNoti.receiver_id).emit('message recieved', newNoti);
+		// socket.in(newNoti.receiver).emit("message recieved", newNoti);
+		// socket.broadcast.emit('message recieved', newNoti.receiver+" " +newNoti.message)
+	});
 
-  socket.off("setup", () => {
-    console.log("USER DISCONNECTED");
-    socket.leave(id);
-  });
+	socket.off('setup', () => {
+		console.log('USER DISCONNECTED');
+		socket.leave(id);
+	});
+});
+
+app.get('/', (req, res) => {
+	res.status(200).send('Hello, world!').end();
 });
 
 sequelize.sync({ force: false }).then(() => {
-  server.listen(PORT, () => console.log('Now listening to port' + PORT));
+	server.listen(PORT, () => console.log('Now listening to port' + PORT));
 });
